befab6a0301c68f1b2744162c834eaa39d3df424
Juan Obando <juanobando.developer@gmail.com>
2023-04-12 15:40:28 -0500

    refactor: upgrade function and work with any dictionary

--- src/hooks/aluraDecoder.js
+++ src/hooks/aluraDecoder.js
@@ -1,8 +1,17 @@
-export const AluraDecoder = (str) => {
-  return str
-    .replace(/ai/g, "a")
-    .replace(/enter/g, "e")
-    .replace(/imes/g, "i")
-    .replace(/ober/g, "o")
-    .replace(/ufat/g, "u");
+const vowels = {
+  a: "ai",
+  e: "enter",
+  i: "imes",
+  o: "ober",
+  u: "ufat",
+};
+
+export const AluraDecoder = (str, dict = vowels) => {
+  let reverseDict = {};
+  for (let key in dict) {
+    reverseDict[dict[key]] = key;
+  }
+
+  let regex = new RegExp(Object.keys(reverseDict).join("|"), "g");
+  return str.replace(regex, (match) => reverseDict[match]);
 };
